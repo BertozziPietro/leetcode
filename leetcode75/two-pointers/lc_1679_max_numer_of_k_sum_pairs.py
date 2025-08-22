56be5fedbcd713a7bf35159c0f7712a5dc12ee5e
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-22 21:49:13 +0200

    feat(two-pointers): solve problem 1679

---
+++ leetcode75/two-pointers/lc_1679_max_numer_of_k_sum_pairs.py
@@ -0,0 +1,7 @@
+import collections
+
+class Solution:
+  def maxOperations(self, nums: list[int], k: int) -> int:
+    count = collections.Counter(nums)
+    return sum(min(count[num], count[k - num])
+               for num in count) // 2
