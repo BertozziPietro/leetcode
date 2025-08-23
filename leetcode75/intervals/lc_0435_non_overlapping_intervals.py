c80d960418349700bb9ad89b70e5012c99b2e0ae
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 20:20:31 +0200

    feat(intervals): solve problem 0435

--- leetcode75/intervals/lc_0435_non_overlapping_intervals.py
+++ leetcode75/intervals/lc_0435_non_overlapping_intervals.py
@@ -2,3 +2,18 @@
 # Study Plan: LeetCode 75
 # Category: Intervals
 # Problem: Non-overlapping Intervals
+
+import math
+
+class Solution:
+  def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+    ans = 0
+    currentEnd = -math.inf
+
+    for interval in sorted(intervals, key=lambda x: x[1]):
+      if interval[0] >= currentEnd:
+        currentEnd = interval[1]
+      else:
+        ans += 1
+
+    return ans
