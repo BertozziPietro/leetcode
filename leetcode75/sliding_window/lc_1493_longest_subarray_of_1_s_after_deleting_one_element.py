a755a5a495eaa749b733995939fada12075202fd
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 18:06:33 +0200

    feat(sliding-window): solve problem 1493

--- leetcode75/sliding_window/lc_1493_longest_subarray_of_1_s_after_deleting_one_element.py
+++ leetcode75/sliding_window/lc_1493_longest_subarray_of_1_s_after_deleting_one_element.py
@@ -2,3 +2,20 @@
 # Study Plan: LeetCode 75
 # Category: Sliding Window
 # Problem: Longest Subarray of 1's After Deleting One Element
+
+class Solution:
+  def longestSubarray(self, nums: list[int]) -> int:
+    ans = 0
+    zeros = 0
+
+    l = 0
+    for r, num in enumerate(nums):
+      if num == 0:
+        zeros += 1
+      while zeros == 2:
+        if nums[l] == 0:
+          zeros -= 1
+        l += 1
+      ans = max(ans, r - l)
+
+    return ans
