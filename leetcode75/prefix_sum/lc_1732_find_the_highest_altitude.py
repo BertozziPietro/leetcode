2efe855a8fce15350ebbeca4c820715e5af260ab
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 16:11:58 +0200

    feat(prefix-sum): solve problem 1732

--- leetcode75/prefix_sum/lc_1732_find_the_highest_altitude.py
+++ leetcode75/prefix_sum/lc_1732_find_the_highest_altitude.py
@@ -2,3 +2,12 @@
 # Study Plan: LeetCode 75
 # Category: Prefix Sum
 # Problem: Find the Highest Altitude
+
+class Solution:
+  def largestAltitude(self, gain: list[int]) -> int:
+    ans = 0
+    currAltitude = 0
+    for g in gain:
+      currAltitude += g
+      ans = max(ans, currAltitude)
+    return ans
