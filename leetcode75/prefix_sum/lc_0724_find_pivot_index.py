d55aecb0124861505b6947bb4d66b3b09ac90ad4
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 16:08:09 +0200

    feat(prefix-sum): solve problem 0724

--- leetcode75/prefix_sum/lc_0724_find_pivot_index.py
+++ leetcode75/prefix_sum/lc_0724_find_pivot_index.py
@@ -2,3 +2,15 @@
 # Study Plan: LeetCode 75
 # Category: Prefix Sum
 # Problem: Find Pivot Index
+
+class Solution:
+  def pivotIndex(self, nums: list[int]) -> int:
+    summ = sum(nums)
+    prefix = 0
+
+    for i, num in enumerate(nums):
+      if prefix == summ - prefix - num:
+        return i
+      prefix += num
+
+    return -1
