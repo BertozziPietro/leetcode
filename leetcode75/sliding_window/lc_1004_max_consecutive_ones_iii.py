b6bc28658b6360b922cf8a7d4b5d37aa299ecd0d
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 17:52:44 +0200

    feat(sliding-window): solve problem 1004

--- leetcode75/sliding_window/lc_1004_max_consecutive_ones_iii.py
+++ leetcode75/sliding_window/lc_1004_max_consecutive_ones_iii.py
@@ -2,3 +2,20 @@
 # Study Plan: LeetCode 75
 # Category: Sliding Window
 # Problem: Max Consecutive Ones III
+
+class Solution:
+  def longestOnes(self, nums: list[int], k: int) -> int:
+
+    ans = 0
+    l = 0
+
+    for r, num in enumerate(nums):
+      if num == 0:
+        k -= 1
+      while k < 0:
+        if nums[l] == 0:
+          k += 1
+        l += 1
+      ans = max(ans, r - l + 1)
+
+    return ans
