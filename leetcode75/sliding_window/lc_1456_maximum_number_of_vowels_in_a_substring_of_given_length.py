e3d45687f3576c9ed38fe6f99ff1acfe7e37ff14
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 17:20:53 +0200

    feat(sliding-window): solve problem 1456

--- leetcode75/sliding_window/lc_1456_maximum_number_of_vowels_in_a_substring_of_given_length.py
+++ leetcode75/sliding_window/lc_1456_maximum_number_of_vowels_in_a_substring_of_given_length.py
@@ -2,3 +2,18 @@
 # Study Plan: LeetCode 75
 # Category: Sliding Window
 # Problem: Maximum Number of Vowels in a Substring of Given Length
+
+class Solution:
+  def maxVowels(self, s: str, k: int) -> int:
+    ans = 0
+    mx = 0
+    kVowels = 'aeiou'
+
+    for i, c in enumerate(s):
+      if c in kVowels:
+        mx += 1
+      if i >= k and s[i - k] in kVowels:
+        mx -= 1
+      ans = max(ans, mx)
+
+    return ans
