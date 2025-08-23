00c7df913b429439eca524c7ff91d60e1d97d1d5
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 20:38:29 +0200

    feat(intervals): solve problem 0452

--- leetcode75/intervals/lc_0452_minimum_number_of_arrows_to_burst_balloons.py
+++ leetcode75/intervals/lc_0452_minimum_number_of_arrows_to_burst_balloons.py
@@ -2,3 +2,17 @@
 # Study Plan: LeetCode 75
 # Category: Intervals
 # Problem: Minimum Number of Arrows to Burst Balloons
+
+import math
+
+class Solution:
+  def findMinArrowShots(self, points: list[list[int]]) -> int:
+    ans = 0
+    arrowX = -math.inf
+
+    for point in sorted(points, key=lambda x: x[1]):
+      if point[0] > arrowX:
+        ans += 1
+        arrowX = point[1]
+
+    return ans
