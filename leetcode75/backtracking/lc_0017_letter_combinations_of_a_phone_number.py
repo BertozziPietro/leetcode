d2bfecd159eb775310b9847fcc9cdb555a847a13
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-23 21:18:15 +0200

    feat(backtracking): solve problem 0017

--- leetcode75/backtracking/lc_0017_letter_combinations_of_a_phone_number.py
+++ leetcode75/backtracking/lc_0017_letter_combinations_of_a_phone_number.py
@@ -2,3 +2,25 @@
 # Study Plan: LeetCode 75
 # Category: Backtracking
 # Problem: Letter Combinations of a Phone Number
+
+class Solution:
+  def letterCombinations(self, digits: str) -> list[str]:
+    if not digits:
+      return []
+
+    digitToLetters = ['', '', 'abc', 'def', 'ghi',
+                      'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+    ans = []
+
+    def dfs(i: int, path: list[str]) -> None:
+      if i == len(digits):
+        ans.append(''.join(path))
+        return
+
+      for letter in digitToLetters[int(digits[i])]:
+        path.append(letter)
+        dfs(i + 1, path)
+        path.pop()
+
+    dfs(0, [])
+    return ans
