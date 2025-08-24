0cede63228fb6eb4f0bdd4fa845d89eaad0983cf
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-24 18:23:21 +0200

    feat(array-string): solve problem 0238

--- leetcode75/array_string/lc_0238_product_of_array_except_self.py
+++ leetcode75/array_string/lc_0238_product_of_array_except_self.py
@@ -2,3 +2,17 @@
 # Study Plan: LeetCode 75
 # Category: Array String
 # Problem: Product of Array Except Self
+
+class Solution:
+  def productExceptSelf(self, nums: list[int]) -> list[int]:
+    n = len(nums)
+    prefix = [1] * n  # prefix product
+    suffix = [1] * n  # suffix product
+
+    for i in range(1, n):
+      prefix[i] = prefix[i - 1] * nums[i - 1]
+
+    for i in reversed(range(n - 1)):
+      suffix[i] = suffix[i + 1] * nums[i + 1]
+
+    return [prefix[i] * suffix[i] for i in range(n)]
