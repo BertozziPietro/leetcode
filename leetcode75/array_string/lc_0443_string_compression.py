49f2c07dd21c208fcf629911757bcbaa8e6a9a13
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-24 18:38:44 +0200

    feat(array-string): solve problem 0443

--- leetcode75/array_string/lc_0443_string_compression.py
+++ leetcode75/array_string/lc_0443_string_compression.py
@@ -2,3 +2,23 @@
 # Study Plan: LeetCode 75
 # Category: Array String
 # Problem: String Compression
+
+class Solution:
+  def compress(self, chars: list[str]) -> int:
+    ans = 0
+    i = 0
+
+    while i < len(chars):
+      letter = chars[i]
+      count = 0
+      while i < len(chars) and chars[i] == letter:
+        count += 1
+        i += 1
+      chars[ans] = letter
+      ans += 1
+      if count > 1:
+        for c in str(count):
+          chars[ans] = c
+          ans += 1
+
+    return ans
