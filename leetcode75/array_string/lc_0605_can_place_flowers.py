35935f4e42c67c2d9071eed66942821410674beb
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-24 17:27:35 +0200

    feat(array-string): solve problem 0605

--- leetcode75/array_string/lc_0605_can_place_flowers.py
+++ leetcode75/array_string/lc_0605_can_place_flowers.py
@@ -2,3 +2,18 @@
 # Study Plan: LeetCode 75
 # Category: Array String
 # Problem: Can Place Flowers
+
+class Solution:
+  def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+    for i, flower in enumerate(flowerbed):
+      if (
+        flower == 0
+        and (i == 0 or flowerbed[i - 1] == 0)
+        and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
+      ):
+        flowerbed[i] = 1
+        n -= 1
+      if n <= 0:
+        return True
+
+    return False
