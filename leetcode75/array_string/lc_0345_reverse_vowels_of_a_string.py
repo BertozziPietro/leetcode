3625437bb471674a8ec33131217a4f3e2a1a9023
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-24 17:30:04 +0200

    feat(array-string): solve problem 0345

--- leetcode75/array_string/lc_0345_reverse_vowels_of_a_string.py
+++ leetcode75/array_string/lc_0345_reverse_vowels_of_a_string.py
@@ -2,3 +2,21 @@
 # Study Plan: LeetCode 75
 # Category: Array String
 # Problem: Reverse Vowels of a String
+
+class Solution:
+  def reverseVowels(self, s: str) -> str:
+    chars = list(s)
+    kVowels = 'aeiouAEIOU'
+    l = 0
+    r = len(s) - 1
+
+    while l < r:
+      while l < r and chars[l] not in kVowels:
+        l += 1
+      while l < r and chars[r] not in kVowels:
+        r -= 1
+      chars[l], chars[r] = chars[r], chars[l]
+      l += 1
+      r -= 1
+
+    return ''.join(chars)
