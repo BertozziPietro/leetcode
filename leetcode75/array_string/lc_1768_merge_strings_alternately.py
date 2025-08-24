e93c281de4069cc3928226608cff1dd53f1a08a7
Pietro Bertozzi <b3rtozzipietro@gmail.com>
2025-08-24 16:02:36 +0200

    feat(array-string): solve problem 1768

--- leetcode75/array_string/lc_1768_merge_strings_alternately.py
+++ leetcode75/array_string/lc_1768_merge_strings_alternately.py
@@ -2,3 +2,9 @@
 # Study Plan: LeetCode 75
 # Category: Array String
 # Problem: Merge Strings Alternately
+
+from itertools import zip_longest
+
+class Solution:
+  def mergeAlternately(self, word1: str, word2: str) -> str:
+    return ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
